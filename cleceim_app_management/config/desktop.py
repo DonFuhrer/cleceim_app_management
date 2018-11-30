0ade7a9f9a8a7e5869bf6a3a11c121276956e6df
vazdan <amazonstoreaya@gmail.com>
2018-11-30 11:09:16 +0100

    modified: config/desktop.py

--- cleceim_app_management/config/desktop.py
+++ cleceim_app_management/config/desktop.py
@@ -4,11 +4,45 @@ from frappe import _
 
 def get_data():
 	return [
+		# module icon 'Ordenes de trabajo'
 		{
 			"module_name": "Ordenes de trabajo",
 			"color": "green",
 			"icon": "octicon octicon-versions",
 			"type": "module",
 			"label": _("\xd3rdenes de trabajo")
+		},
+
+		# doctype icon 'Gran Formato'
+		{
+			"module_name": "Gran Formato",
+			"color": "green",
+			"icon": "octicon octicon-tools",
+			"type": "list",
+			"link": "List/Gran Formato",
+			"doctype": "Gran Formato",
+			"label": "Gran Formato"
+		},
+
+		# doctype icon 'Digital'
+		{
+			"module_name": "Digital",
+			"color": "green",
+			"icon": "octicon octicon-paintcan",
+			"type": "list",
+			"link": "List/Digital",
+			"doctype": "Digital",
+			"label": "Digital"
+		},
+
+		# doctype icon 'Textil'
+		{
+			"module_nam": "Textil",
+			"color": "green",
+			"icon": "octicon octicon-jersey",
+			"type": "list",
+			"link": "List/Textil",
+			"doctype": "Textil",
+			"label": "Textil"
 		}
 	]
